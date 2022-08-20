59a06fba4e743c4662fc3e5dd1c8d8da86abe4ac
CRALPURO <cralpuro@gmail.com>
2022-08-20 13:54:16 -0500

    Añado Talle5.py

---
+++ Talle6.py
@@ -0,0 +1,20 @@
+# ------------------------------------------     Taller Metodos numericos   --------------------------------------------------------------#
+#6
+
+#Definición de librerias
+import math as mt 
+
+#Definición de varibales
+valor_e = 0
+limite = 100
+
+
+#Main 
+
+x = int(input("Diguiete la potencia de euler: "))
+
+for n in range(limite):
+    valor_e += mt.pow(x,n)/mt.factorial(n)
+
+print("El valor de euler por funciones de python es:", mt.pow(mt.e,x))
+print("El valor de euler por logica de taylor es:",valor_e)
